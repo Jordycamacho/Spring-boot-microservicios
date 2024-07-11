12ae6f9ba71643195a1413a1cfd50481a174ca67
Jordy Camacho <jordycamacho225@gmail.com>
2024-07-11 21:54:03 +0200

    [imp] configuracion de seguridad terminado

---
+++ api-gateway/src/main/java/com/example/api_gateway/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.api_gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+            .oauth2Login(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
