f8021d029de1f2c44e2c4bb6f3845a159ff6f619
Jordy Camacho <jordycamacho225@gmail.com>
2024-07-12 12:46:48 +0200

    seguridad no terminada

---
+++ product-microservice/src/main/java/com/example/product_microservice/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.product_microservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated())
+            .oauth2Login(withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
